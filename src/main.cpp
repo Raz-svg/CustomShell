bf0a62e17cfc85f325c93d360e52ab7755b88a33
Raz-svg <sur37704@gmail.com>
2026-08-14 23:11:54 +0530

    echo command

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,42 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+void echo(std::vector<std::string> &tokens){
+    for (int i=1 ;i< tokens.size();i++){
+        std::cout<< tokens[i];
+        if(i+1 < tokens.size()){
+            std::cout<< " ";
+        }
+    }
+
+    std::cout<< "\n";
+
+}
+
+std::vector<std::string> tokenize(std::string& input){
+
+    std::vector<std::string>tokens;
+    std::string current_token = "";
+
+    for (int i=0; i<std::size(input);i++){
+        if(input[i] == ' '){
+            if(!current_token.empty()){
+                tokens.push_back(current_token);
+                current_token.clear();
+            }
+        }
+        else{
+            current_token += input[i];
+        }
+    }
+    if(!current_token.empty()){
+        tokens.push_back(current_token);
+    }
+
+    return tokens;
+
+}
 
 int main() {
   // Flush after every std::cout / std:cerr
@@ -12,11 +49,20 @@ int main() {
       std::cout<< "$ ";
       std::getline(std::cin,input);
 
-      if(input =="exit"){
+      std::vector<std::string>command = tokenize(input);
+
+      if(command.empty()){
+          continue;
+      }
+
+      if(command[0] =="exit" && (command.size()==1)){
           break;
       }
+      else if(command[0]=="echo" && (command.size()>=1)){
+          echo(command);
+      }
       else{
-           std::cout<< input<<": command not found\n";
+           std::cout<<command[0]<<": command not found\n";
       }
 
   }
